8def149566adf1c5628a16c4274ed01c7be55d83
JadweR <jadwer@msn.com>
2025-04-12 10:32:54 -0600

    agregamos el modelo de los átomos, con núcleo y órbitas. Solucionamos Zoom de la cámara del teléfono

--- js/Compounds.js
+++ js/Compounds.js
@@ -2,6 +2,7 @@ AFRAME.registerComponent('atoms', {
 
     init: async function () {
       // Do something when component first attached.
+      console.log("Init Function: Cargando elementos..." )
       var compounds = await this.getCompounds();
       var barcodes = Object.keys(compounds); // [0,1]
       barcodes.map(barcode => {
@@ -15,20 +16,20 @@ AFRAME.registerComponent('atoms', {
     },
 
 
-    getCompounds: function () {
+    getCompounds: async function () {
         //Obtener los detalles de la composición del archivo JSON
-        return fetch('json/compounds.json')
-        .then(res => res.json())
-        .then(data => data);
+        const res = await fetch('js/compoundList.json');
+      const data = await res.json();
+      return data;
     },
-    getElementColors: function () {
+    getElementColors: async function () {
         //Obtener el color del elemento del archivo JSON
-        return fetch('json/elementColors.json')
-        .then(res => res.json())
-        .then(data => data);
+        const res = await fetch('js/elementColors.json');
+      const data = await res.json();
+      return data;
     },
     createAtoms: async function (element) {
-        console.log("hola")
+        console.log("CreateAtomos Function: " + element.element_name);
         //Datos del elemento
         var elementName = element.element_name;
         var barcodeValue = element.barcode_value;
@@ -50,13 +51,155 @@ AFRAME.registerComponent('atoms', {
 
         scene.appendChild(marker);
 
+        var atom = document.createElement('a-entity');
+        atom.setAttribute('id', `${elementName}-${barcodeValue}`);
+        marker.appendChild(atom);
+
         //Crear tarjeta de atomo
+        var card = document.createElement('a-entity');
+        card.setAttribute('id', `card-${elementName}`);
+        card.setAttribute('geometry', {
+            primitive: 'plane',
+            height: 1,
+            width: 1
+        });
+
+        card.setAttribute('material', {
+            src: `assets/atom_cards/card_${elementName}.png`,
+        });
+
+        card.setAttribute('position', {
+            x: 0,
+            y: 0,
+            z: 0
+        });
+        card.setAttribute('rotation', {
+            x: -90,
+            y: 0,
+            z: 0
+        });
+
+        atom.appendChild(card);
 
         //Crear núcleo
+        var nucleusRadius = 0.2;
+        var nucleus = document.createElement('a-entity');
+        nucleus.setAttribute('id', `nucleus-${elementName}`);
+        nucleus.setAttribute('geometry', {
+            primitive: 'sphere',
+            radius: nucleusRadius
+        });
+
+        nucleus.setAttribute('material', "color", colors[elementName]);
+        nucleus.setAttribute('position', {
+            x: 0,
+            y: 1,
+            z: 0
+        });
+        nucleus.setAttribute('rotation', {
+            x: 0,
+            y: 0,
+            z: 0
+        });
+
+        var nucleusName = document.createElement('a-entity');
+        nucleusName.setAttribute('id', `nucleus-name-${elementName}`);
+        nucleusName.setAttribute('position', {
+            x: 0,
+            y: 0.21,
+            z: -0.06
+        });
+        nucleusName.setAttribute('rotation', {
+            x: -90,
+            y: 0,
+            z: 0
+        });
+        nucleusName.setAttribute('text', {
+            font: 'monoid',
+            width: 3,
+            color: 'black',
+            align: 'center',
+            value: elementName
+        });
+        nucleus.appendChild(nucleusName);
+        atom.appendChild(nucleus);
 
         //Crear órbita para la revolución del electrón
+        var orbitAngle = 180;
+        var electronAngle = 30;
 
-        //Crear entidad de animación de la revolución de los electrones
+        for (var num = 1; num <= numOfElectron; num++) {
+          var orbit = document.createElement('a-entity');
+          orbit.setAttribute('id', `orbit-${num}`);
+          orbit.setAttribute('geometry', {
+              primitive: 'torus',
+              arc: 360,
+              radius: 0.28,
+              radiusTubular: 0.003,
+          });
 
+          orbit.setAttribute('material', {
+              color: '#ff9e80',
+              opacity: 0.3,
+          });
+
+          orbit.setAttribute('position', {
+              x: 0,
+              y: 1,
+              z: 0
+          });
+
+          orbit.setAttribute('rotation', {
+              x: 0,
+              y: orbitAngle,
+              z: 0
+          });
+
+          orbitAngle += 45;
+
+          atom.appendChild(orbit);
+          
+          //Crear entidad de animación de la revolución de los electrones
+          
+          var electronGroup = document.createElement('a-entity');
+          electronGroup.setAttribute('id', `electron-group-${elementName}`);
+          electronGroup.setAttribute('rotation', {
+            x: 0,
+            y: 0,
+            z: electronAngle
+          });
+          
+          electronAngle += 65;
+
+          // animacion
+          electronGroup.setAttribute('animation', {
+            property: 'rotation',
+            to: `0 0 -360`,
+            loop: true,
+            dur: 3500,
+            easing: 'linear',
+          });
+
+          // crear electron
+          var electron = document.createElement('a-entity');
+          electron.setAttribute('id', `electron-${elementName}`);
+          electron.setAttribute('geometry', {
+              primitive: 'sphere',
+              radius: 0.02
+          });
+
+          electron.setAttribute('material', {
+              color: "#0d47a1",
+              opacity: 0.6,
+          });
+          electron.setAttribute('position', {
+              x: 0.2,
+              y: 0.2,
+              z: 0
+          });
+
+          electronGroup.appendChild(electron);
+          orbit.appendChild(electronGroup);
+        } // end for
     }
 });
